9eb28efea2d1da96ed764e70571520fc4949a21e
arjuna8 <63253544+arjuna8@users.noreply.github.com>
2026-06-29 17:18:01 +0300

    Update avito_parser.py

--- avito_parser.py
+++ avito_parser.py
@@ -7,19 +7,16 @@ def scrape_avito_rental_prices(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Находим все объявления о студиях в Даниловском районе
     studio_ads = soup.select('.item.js-item-serp-card__link')
     prices = []
 
     for ad in studio_ads:
-        # Получаем ссылку на объявление
         link = ad['href']
 
-        # Отправляем запрос на страницу объявления
         ad_response = requests.get(link, headers=headers)
         ad_soup = BeautifulSoup(ad_response.content, 'html.parser')
 
-        # Находим цену аренды на странице объявления
+        
         price_element = ad_soup.select_one('.price')
         if price_element:
             price_text = price_element.text.strip()
